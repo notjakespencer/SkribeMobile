6496ce98be3bb9e5d59780cc959d2d6aa07d98b2
notjakespencer <jake@jakespencer.co>
2025-12-27 16:06:20 -0800

    Update accessibility props & Android status bar handling
    Replaced AutomationProperties.Name with SemanticProperties.Description in XAML for improved accessibility and .NET MAUI best practices. Refactored Android MainActivity to use modern status bar and system UI APIs, ensuring better compatibility across Android versions. Bumped app version to 1.0.2 (version code 4).

--- SkribeMaui/Platforms/Android/MainActivity.cs
+++ SkribeMaui/Platforms/Android/MainActivity.cs
@@ -2,21 +2,54 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
-using Android.Graphics;
 
 namespace SkribeMaui.Platforms.Android
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(
+        Theme = "@style/Maui.SplashTheme",
+        MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges =
+            ConfigChanges.ScreenSize |
+            ConfigChanges.Orientation |
+            ConfigChanges.UiMode |
+            ConfigChanges.ScreenLayout |
+            ConfigChanges.SmallestScreenSize |
+            ConfigChanges.Density)]
+
+
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)(SystemUiFlags.LayoutStable | SystemUiFlags.LayoutFullscreen);
-            Window.SetStatusBarColor(global::Android.Graphics.Color.Transparent);
+            if (OperatingSystem.IsAndroidVersionAtLeast(30) &&
+                !OperatingSystem.IsAndroidVersionAtLeast(35))
+            {
+                Window?.SetDecorFitsSystemWindows(false);
+            }
 
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.R) Window.SetDecorFitsSystemWindows(false);
+            if (OperatingSystem.IsAndroidVersionAtLeast(30))
+            {
+                var controller = Window?.InsetsController;
+                if (controller != null)
+                {
+                    controller.Hide(WindowInsets.Type.StatusBars());
+                    controller.SystemBarsBehavior =
+                        (int)WindowInsetsControllerBehavior.ShowTransientBarsBySwipe;
+                }
+            }
+            else
+            {
+                var decor = Window?.DecorView;
+                if (decor != null)
+                {
+                    decor.SystemUiFlags = SystemUiFlags.LayoutStable |
+                                          SystemUiFlags.LayoutFullscreen |
+                                          SystemUiFlags.Fullscreen;
+                }
+            }
         }
     }
 }
